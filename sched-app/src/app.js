c0875be0dbd5fbce80c61ec650d307d986e75695
Michael Purcaro <purcaro@gmail.com>
2018-07-16 18:52:31 -0400

    a simple date picker

--- sched-app/src/app.js
+++ sched-app/src/app.js
@@ -1,31 +1,57 @@
 import React, { Component } from 'react';
 import './app.css';
-import { Segment } from 'semantic-ui-react'
+import { Message, Grid } from 'semantic-ui-react';
+
+import { DateInput } from 'semantic-ui-calendar-react';
+import '../node_modules/semantic-ui-calendar-react/dist/css/calendar.min.css';
+
+class DateSelect extends React.Component {
+    state = { date: 'July 5, 2018' };
+
+    handleChange = (event, {name, value}) => {
+        if (this.state.hasOwnProperty(name)) {
+            console.log(name, value);
+            this.setState({ [name]: value });
+        }
+    }
+
+    render() {
+        return (
+            <DateInput
+              name="date"
+              placeholder="Date"
+              value={this.state.date}
+              iconPosition="left"
+              dateFormat="MMM DD, YYYY"
+              onChange={this.handleChange} />
+        );
+    }
+}
 
 class App extends Component {
     render() {
 	return (
-	    <React.Fragment>
-	      <Segment.Group raised>
-		<Segment>
-		  <h4>Pick Start Date</h4>
-		  <p>
-		    <strong>1)</strong>
-		    First pick first day of rotations not including transition course, the rest of the dates will be automatically generated
-		  </p>
-		  <br></br>
-		  <p>
-		    <strong>2)</strong>
-		    Then download the schedule below after picking rotation orders
-		  </p>
-		  <div id="start_date" className="shiny-date-input form-group shiny-input-container shiny-bound-input">
-		    <label className="control-label" for="start_date">Start Date (default is 2018-2019 start date):</label>
-		    <input type="text" className="form-control" data-date-language="en" data-date-week-start="0" data-date-format="yyyy-mm-dd" data-date-start-view="month" data-initial-date="2018-05-07">
-		    </input>
-		  </div>
-		</Segment>
-	      </Segment.Group>
-	    </React.Fragment>
+            <Grid>
+              <Grid.Row>
+                <Grid.Column width={3}>
+                  <Message icon>
+                    <Message.Content>
+                      <Message.Header>Pick Start Date</Message.Header>
+		      <p>
+		        <strong>1)</strong>
+		        First pick first day of rotations not including transition course, the rest of the dates will be automatically generated
+		      </p>
+		      <p>
+		        <strong>2)</strong>
+		        Then download the schedule below after picking rotation orders
+		      </p>
+		      <label>Start Date (default is 2018-2019 start date):</label>
+                      <DateSelect />
+                    </Message.Content>
+                  </Message>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
 );
 }
 }
