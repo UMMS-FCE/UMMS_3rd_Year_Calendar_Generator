0270f468047945b2b496f7031df606d1c686b88f
Michael Purcaro <purcaro@gmail.com>
2018-07-16 18:58:29 -0400

    more semantic

--- sched-app/src/app.js
+++ sched-app/src/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './app.css';
-import { Message, Grid } from 'semantic-ui-react';
+import { Message, Grid, Header, List } from 'semantic-ui-react';
 
 import { DateInput } from 'semantic-ui-calendar-react';
 import '../node_modules/semantic-ui-calendar-react/dist/css/calendar.min.css';
@@ -28,27 +28,41 @@ class DateSelect extends React.Component {
     }
 }
 
+class StartDate extends Component {
+    render() {
+	return (
+            <Message icon>
+              <Message.Content>
+                <Message.Header>Pick Start Date</Message.Header>
+		<List ordered>
+                  <List.Item>
+		    First pick first day of rotations not including transition course, the rest of the dates will be automatically generated
+                  </List.Item>
+                  <List.Item>
+		    Then download the schedule below after picking rotation orders
+                  </List.Item>
+                </List>
+		<label>Start Date (default is 2018-2019 start date):</label>
+                <DateSelect />
+              </Message.Content>
+            </Message>
+);
+}
+}
+
 class App extends Component {
     render() {
 	return (
             <Grid>
               <Grid.Row>
+                <Grid.Column width={13}>
+                  <Header as='h1'>UMMS 3rd Year Calendar Generator</Header>
+                </Grid.Column>
+              </Grid.Row>
+
+              <Grid.Row>
                 <Grid.Column width={3}>
-                  <Message icon>
-                    <Message.Content>
-                      <Message.Header>Pick Start Date</Message.Header>
-		      <p>
-		        <strong>1)</strong>
-		        First pick first day of rotations not including transition course, the rest of the dates will be automatically generated
-		      </p>
-		      <p>
-		        <strong>2)</strong>
-		        Then download the schedule below after picking rotation orders
-		      </p>
-		      <label>Start Date (default is 2018-2019 start date):</label>
-                      <DateSelect />
-                    </Message.Content>
-                  </Message>
+                  <StartDate />
                 </Grid.Column>
               </Grid.Row>
             </Grid>
